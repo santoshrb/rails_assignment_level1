11390aec5b2c8bb98bc145599afe1426dc53ac39
Santosh Kumbhar <kumbharsansk@gmail.com>
2016-12-26 10:12:04 +0530

    Updated generate_promocode method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,21 @@ class User < ApplicationRecord
   before_validation :generate_promocode, on: :create
   validates :name, :promo_code, presence: true
 
-	def generate_promocode
-  		promo_code = ""
-		base_value = 65;
-		for i in 1..3 
-	  		pw = (base_value + rand(26).to_i).chr
-		  	promo_code << pw 
-		end
-		
-		promo_code << 5.times do rand(10)
+  def generate_promocode
+	promo_code = ""
+	base_value = 65;
+	for i in 1..3 
+	  pw = (base_value + rand(26).to_i).chr
+	  promo_code << pw 
+	end
 
-		self.promo_code = promo_code
-  	end
+	int_value = 48
 
+	for i in 1..4
+		a = (int_value + rand(10)).chr
+		promo_code << a
+	end
+	
+	self.promo_code = promo_code
+  end
 end
